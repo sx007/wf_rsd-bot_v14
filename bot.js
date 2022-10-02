83082fa999a11318b3a75f191b38e60d2f2d7a52
Ivan <42902953+sx007@users.noreply.github.com>
2022-10-02 14:34:17 +1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,44 @@
+const { Client, GatewayIntentBits, Partials, EmbedBuilder, ActionRowBuilder, SelectMenuBuilder, ButtonBuilder, ButtonStyle, SlashCommandBuilder, TextInputBuilder, ModalBuilder, ContextMenuCommandBuilder, ApplicationCommandType } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildBans, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.MessageContent], partials: [Partials.User, Partials.Message, Partials.Channel, Partials.Reaction, Partials.GuildMember, Partials.GuildScheduledEvent, Partials.ThreadMember] });
+var request = require('request');
+//Токен
+const token = process.env.BOT_TOKEN;
+//Префикс для команд
+const prefix = process.env.PREFIX;
+//ID канала, куда слать системные сообщения
+const idChMsg = process.env.ID_CHANNEL_SEND;
+//ID сервера
+const idSrv = process.env.ID_SERVER;
+//Название клана
+const clNm = process.env.CLAN_NAME;
+//ID ролей (Администраторов и Модераторов)
+const idAdmMod = process.env.ID_ADM_MOD_ROLE;
+//Время старта бота
+const startBot = Date.now();
+//Для устранения проблем с получением request
+process.env.UV_THREADPOOL_SIZE = 64;
+//Задаем настройки по умолчанию
+const customRequest = request.defaults({
+    method: "GET",
+    agent : false, 
+    pool : {maxSockets: 500}, 
+    timeout : 20000
+});
 
+
+//
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', (message) => {
+  //Если это сам же бот, то игнорировать
+  if (message.author.bot) return;
+  
+  if (message.content === 'ping'){
+      message.reply('Pong!')
+  }
+});
+
+client.login(token);
